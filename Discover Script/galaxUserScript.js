bd9a170ec8cd310bb23e8fc5ac07920332ee7b54
RatanAtHcl <ratan_kumar@hcl.com>
2022-12-02 13:21:48 +0530

    from local to DBA

--- Discover Script/galaxUserScript.js
+++ Discover Script/galaxUserScript.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Galax userScript
-// @namespace    https://www.galax.co
+// @namespace    https://www.puntoblanco.co/
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.galax.co/*
+// @match        https://www.puntoblanco.co/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
+alert('dddd')
 
 // events should be of type Array
 function addMultipleDCXListeners(element,events,handler,useCapture){
@@ -23,8 +24,17 @@ function addMultipleDCXListeners(element,events,handler,useCapture){
 
 function DCXHandler(e) {
     if (e.target.id == "input" && e.target.value.length >3){
-        alert('srchBx is '+ e.target.value);
-        // do your things here ...
+        // Create New Event
+        const newEvent = new Event('change', { bubbles: true });
+        // Creste New Input
+        const newInput = document.createElement('input');
+        newInput.value = e.target.value;
+        // Listen for the event.
+        newInput.addEventListener('change', (ee) => { alert(ee.target.value)})
+        // Dispatch the event.
+        newInput.dispatchEvent(newEvent);
+        // Remove newley created Input
+        newInput.remove()
     }
 };
 
@@ -40,14 +50,19 @@ window.addEventListener("load", (event) => {
             // then add Mult event listeners
             addMultipleDCXListeners(
                 document.querySelector('#input'),
-                ['mousedown','change'],
+                ['mouseup'],
                 DCXHandler,
                 false);
         } else if(cnt >= 40) {
-            // clear Interval if finding count is greater then 5.
+            // clear Interval if page loading time is greater then 120 Sec.
             clearInterval(DXCInterval);
         }
     }, 3000)
 });
+
+
+
+
+
 
 
